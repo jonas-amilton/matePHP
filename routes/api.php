aee2f0dbdf7668420ec705a32b05e1f5dd121382
jonas-amilton <jonas96@edu.unisinos.br>
2025-11-11 22:20:38 -0300

    docs(routes): adicionar bloco PHPDoc com exemplos de rotas e limpar definições da API

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,22 @@
 <?php
 
-
-use Framework\Core\Router;
+use Framework\Core\Route;
 use App\Http\Controllers\ExampleController;
 
+/**
+ * ------------------------------------------------------------
+ *  API Routes
+ * ------------------------------------------------------------
+ * Here you can define all API endpoints for your application.
+ * 
+ * Examples:
+ *  - Route::get('/users', [UserController::class, 'index']);
+ *  - Route::post('/users', [UserController::class, 'store']);
+ *  - Route::resource('posts', PostController::class);
+ *  - Route::group(['prefix' => 'admin'], function ($r) {
+ *        $r->get('/dashboard', [AdminController::class, 'index']);
+ *    });
+ */
 
-$router = new Router();
-
-
-$router->get('/api/hello', [ExampleController::class, 'hello']);
-$router->get('/api/users', [ExampleController::class, 'index']);
-
-
-return $router;
+Route::get('/api/hello', [ExampleController::class, 'hello']);
+Route::get('/api/users', [ExampleController::class, 'index']);
